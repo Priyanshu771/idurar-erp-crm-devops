f29a8dc68922507aa52d803a28da3dec693f874b
Priyanshu771 <priyanshu.sahu@cylsys.com>
2025-11-25 16:18:02 +0530

    Demo

--- frontend/src/apps/IdurarOs.jsx
+++ frontend/src/apps/IdurarOs.jsx
@@ -65,7 +65,7 @@ export default function IdurarOs() {
     return (
       <>
       <h1 style={{textAlign:'center', color:'red'}}>
-        🚀 CI/CD — Successfully! HIPHIP hurry
+        🚀 CI/CD — Hello MR softwares!
       </h1>
 
       <Localization>
